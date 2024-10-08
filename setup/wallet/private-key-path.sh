327d20c223b1258181ab2348da6cc4b9c2fa7ff9
TesterBoy <152272581+CryptoBuroMaster@users.noreply.github.com>
2024-10-08 22:33:51 +0530

    Update private-key-path.sh

--- setup/wallet/private-key-path.sh
+++ setup/wallet/private-key-path.sh
@@ -1,5 +1,9 @@
-# To read the private key and format it with spaces:
+# Read the private key without adding any spaces or formatting
+PRIVATE_KEY=$(cat /root/.story/story/config/private_key.txt | sed 's/PRIVATE_KEY=//')
 
-PRIVATE_KEY=$(cat /root/.story/story/config/private_key.txt | sed 's/../& /g')
+# Save the plain private key to private-key-path.sh for use in other scripts
 echo "PRIVATE_KEY=\"$PRIVATE_KEY\"" > private-key-path.sh
+
+# Specify the path to the private key file
 echo 'PRIVATE_KEY_PATH="/root/.story/story/config/private_key.txt"' >> private-key-path.sh
+
