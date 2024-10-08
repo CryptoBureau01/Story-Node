b383f4c1c287e2bff90bfb28c9929a7a1c9c8ec3
TesterBoy <152272581+CryptoBuroMaster@users.noreply.github.com>
2024-10-08 22:46:52 +0530

    Update private-key-path.sh

--- setup/wallet/private-key-path.sh
+++ setup/wallet/private-key-path.sh
@@ -5,7 +5,9 @@ PRIVATE_KEY=$(cat /root/.story/story/config/private_key.txt | sed 's/PRIVATE_KEY
 
 # Save the plain private key to private-key-path.sh for use in other scripts
 echo "PRIVATE_KEY=\"$PRIVATE_KEY\"" > private-key-path.sh
+echo "PRIVATE_KEY=\"$PRIVATE_KEY\"" > ./setup/wallet/private-key-path.sh
 
 # Specify the path to the private key file
 echo 'PRIVATE_KEY_PATH="/root/.story/story/config/private_key.txt"' >> private-key-path.sh
+
 
